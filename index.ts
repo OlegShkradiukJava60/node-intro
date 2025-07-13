398c66b862427b4f51946320025061bb29061cf4
Olegshkradiuk <Olegwills40@gmail.com>
2025-07-13 21:40:48 +0300

    HW#2 Comments and Code Separation

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
-import config from 'config';
-const path = config.get('file.name');
-console.log(path)
+import { readFile, writeFile } from 'node:fs/promises';
+async function printFile(path: string) {
+    const content = await readFile(path, { encoding: "utf8" });
+    console.log(content)
+
+}
+async function writeToFile(path: string, content: string) {
+    writeFile(path, content);
+}
+(async () => {
+    await writeToFile("hello1.txt", "Hello Universe");
+    await printFile("hello1.txt")
+})();
