2be326da35d1ddadac3d579b7111c13af96c937a
Olegshkradiuk <Olegwills40@gmail.com>
2025-07-29 09:20:50 +0300

    hw 5 solution

--- index.ts
+++ index.ts
@@ -1,40 +1,53 @@
-import RandomNumbersStream from "./RandomNumbersStream.ts";
-import config from 'config'
+
+import displayUniqueRandomNumbers from "./display_unique_numbers.ts";
+import config from "config";
 interface Params {
-    amount: number;
-    minValue: number;
-    maxValue: number;
+  minValue: number;
+  maxValue: number;
+  amount: number;
+  delimiter: string
 }
-const AMOUNT_CONFIG_NAME = "amount";
 const MIN_VALUE_CONFIG_NAME = "min";
 const MAX_VALUE_CONFIG_NAME = "max";
-const DEFAULT_AMOUNT_VALUE = 7;
+const AMOUNT_VALUE_CONFIG_NAME = "amount";
+const DELIMITER_VALUE_CONFIG_NAME = "delimiter";
+ const DEFAULT_DELIMITER_VALUE = "; ";
 const DEFAULT_MIN_VALUE = 1;
 const DEFAULT_MAX_VALUE = 49;
+const DEFAULT_AMOUNT = 7;
 function getParams(): Params {
-    const amount: number = getParam(AMOUNT_CONFIG_NAME, DEFAULT_AMOUNT_VALUE);
-    const minValue: number = getParam(MIN_VALUE_CONFIG_NAME, DEFAULT_MIN_VALUE);
-    const maxValue: number = getParam(MAX_VALUE_CONFIG_NAME, DEFAULT_MAX_VALUE);
-    return {amount, minValue, maxValue};
-
+  const minValue = getParamNumber(MIN_VALUE_CONFIG_NAME, DEFAULT_MIN_VALUE);
+  const maxValue = getParamNumber(MAX_VALUE_CONFIG_NAME, DEFAULT_MAX_VALUE);
+  const amount = getParamNumber(AMOUNT_VALUE_CONFIG_NAME, DEFAULT_AMOUNT);
+  const delimiter = getParamString(DELIMITER_VALUE_CONFIG_NAME, DEFAULT_DELIMITER_VALUE);
+  return { minValue, maxValue, amount, delimiter };
 }
-function getParam(configName: string, defaultValue: number): number {
-    return config.has(configName) ? config.get<number>(configName) : defaultValue;
+function getParamNumber(configName: string, defaultValue: number): number {
+  const res: number = config.has(configName)
+    ? config.get<number>(configName)
+    : defaultValue;
+  if (typeof res != "number") {
+    throw new Error(
+      `Value of ${configName} parameter is "${res}" but must be a number`
+    );
+  }
+  return res;
+} 
+function getParamString(configName: string, defaultValue: string): string {
+     const res: string = config.has(configName)
+    ? config.get<string>(configName)
+    : defaultValue;
+  if (typeof res != "string") {
+    throw new Error(
+      `Value of ${configName} parameter is "${res}" but must be a string`
+    );
+  }
+  return res;
 }
-async function displayUniqueRandomNumbers({amount, minValue, maxValue}): Promise<void> {
-
-    return new Promise((resolve, reject) => {
-        try {
-            const stream = new RandomNumbersStream(amount, minValue, maxValue, true);
-            stream.pipe(process.stdout);
-            stream.on("end", () => {
-                    process.stdout.write("\n");
-                    resolve();
-            })
-            stream.on("error", (err) => reject(err))
-        } catch(err) {
-            reject(err)
-        }
-    })
-}
-displayUniqueRandomNumbers(getParams()).catch(err => console.log(err.message))
+(async () => {
+  try {
+    await displayUniqueRandomNumbers(getParams());
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
